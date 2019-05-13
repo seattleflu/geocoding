370902212e5be4414c56eaeae5017542f36430e7
Kairsten Fay <kairsten.fay@gmail.com>
2019-05-13 13:50:17 -0700

    Checkpoint

--- src/address_to_census_tract.py
+++ src/address_to_census_tract.py
@@ -187,7 +187,7 @@ def process_csv_or_excel(file_path: str, address_map: dict):
     
     # Drop identifiable address columns
     df = df.drop(columns=address_columns + ['lat', 'lng'])
-    df.to_csv('data/test/out.csv', index=False) 
+    print(df.to_csv(index=False)) 
 
 def lookup_address(address: dict, address_map: dict) -> dict:
     """
@@ -199,11 +199,7 @@ def lookup_address(address: dict, address_map: dict) -> dict:
     broken into pieces (street, city, zipcode, etc.) or is a free text lookup 
     (and only the `street` parameter is used).
     """
-    auth_id = os.environ['SMARTYSTREETS_AUTH_ID']
-    auth_token = os.environ['SMARTYSTREETS_AUTH_TOKEN']
-
-    credentials = StaticCredentials(auth_id, auth_token)
-    client = ClientBuilder(credentials).build_us_street_api_client()
+    client = smartystreets_client_builder().build_us_street_api_client()
     
     lookup = us_street_lookup(address, address_map)
     if not lookup.street:
@@ -213,49 +209,8 @@ def lookup_address(address: dict, address_map: dict) -> dict:
         """))
         return
 
-def process_csv(file_path: str):
-    """
-    Given a *file_path* to a CSV, processes the `address` column and adds 
-    extra columns for a standardized address, latitude and longitude
-    coordinates, and census tract. If a given address is invalid, these new 
-    columns are left blank. Dumps the new table to stdout. 
+        lookup = Lookup()
 
-    To minimize costs, an address should only be looked up once (via 
-    `lookup_address()`).
-    """
-    df = pd.read_csv(file_path)
-    tracts = load_geojson("data/geojsons/Washington_2016.geojson")
-
-    response = pd.Series(df['address'].apply(lookup_address))
-
-    # Parse response object to create columns of interest
-    df['standard_address'] = response.apply(lambda x: x and x['standard_address'])
-    df['lat'] = response.apply(lambda x: x and x['lat'])
-    df['lng'] = response.apply(lambda x: x and x['lng'])
-
-    latlng = pd.Series(list(zip(df['lat'], df['lng'])))
-    df['final_tract'] = latlng.apply(lambda x: latlng_to_polygon(x, tracts))
-    
-    print(df.to_csv(index=False)) 
-
-def smartystreets_client_builder():
-    """
-    Returns a new :class:`smartystreets_python_sdk.ClientBuilder` using
-    credentials from the environment variables ``SMARTYSTREETS_AUTH_ID`` and
-    ``SMARTYSTREETS_AUTH_TOKEN``.
-    """
-    auth_id = os.environ['SMARTYSTREETS_AUTH_ID']
-    auth_token = os.environ['SMARTYSTREETS_AUTH_TOKEN']
-
-    return ClientBuilder(StaticCredentials(auth_id, auth_token))
-
-def lookup_address(address: str) -> dict:
-    """
-    Given an address, returns a dict containing a standardized address and 
-    lat/long coordinates from SmartyStreet's US Street geocoding API.
-    """
-
-    lookup = Lookup()
     lookup.street = address
     lookup.candidates = 1
     lookup.match = "Invalid"  # Most permissive
@@ -272,7 +227,7 @@ def lookup_address(address: str) -> dict:
         No match found for given address. Extracting address from text
         """))
         address_values = ', '.join([ str(val) for val in list(address.values()) if val ])
-        result = extract_address(credentials, address_values) 
+        result = extract_address(address_values) 
         if not result:
             LOG.warning(dedent(f"""
             Could not look up address {address}.
@@ -281,6 +236,17 @@ def lookup_address(address: str) -> dict:
 
     return {"lat": result[0].metadata.latitude,
             "lng": result[0].metadata.longitude}
+
+def smartystreets_client_builder():
+    """
+    Returns a new :class:`smartystreets_python_sdk.ClientBuilder` using
+    credentials from the environment variables ``SMARTYSTREETS_AUTH_ID`` and
+    ``SMARTYSTREETS_AUTH_TOKEN``.
+    """
+    auth_id = os.environ['SMARTYSTREETS_AUTH_ID']
+    auth_token = os.environ['SMARTYSTREETS_AUTH_TOKEN']
+
+    return ClientBuilder(StaticCredentials(auth_id, auth_token))
 
 def us_street_lookup(address: dict, api_map: dict) -> Lookup:
     """
@@ -308,18 +274,17 @@ def us_street_lookup(address: dict, api_map: dict) -> Lookup:
     lookup.match = "Invalid"  # Most permissive
     return lookup
     
-def extract_address(credentials, text: str):
+def extract_address(text: str):
     """
-    Given arbitrary *text* and *credentials* to SmartyStreets' geocoding APIs, 
-    returns a result from the SmartyStreets US Extract API containing 
-    information about an address connected to the text.
+    Given arbitrary *text*, returns a result from the SmartyStreets US Extract 
+    API containing information about an address connected to the text.
 
     Note that this API is not consistent with the US Street API, and the lookup
     and responses must be handled differently.
 
     Assumes addresses are given in the correct order # TODO enforce
     """
-    client = ClientBuilder(credentials).build_us_extract_api_client()
+    client = smartystreets_client_builder().build_us_extract_api_client()
     lookup = ExtractLookup()
     lookup.text = text
 
@@ -333,7 +298,7 @@ def latlng_to_polygon(latlng: list, polygons):
     Find the first polygon in *polygons* (loaded from file) which contains the 
     *latlng* and return the polygon, else None.
     """
-    
+
     # Ye olde lat/lng vs. lng/lat schism rears its head.
     lat, lng = latlng
 
@@ -348,47 +313,6 @@ def latlng_to_polygon(latlng: list, polygons):
     Failed to find tract for {latlng}.
     """))
     return None
-    if not result:  # Invalid address
-        result = extract_address(address)
-        if not result:
-            LOG.warning(f"Could not look up address {address}")
-            return 
-
-    first_candidate = result[0]
-
-    return {"standard_address": standard_address(first_candidate),
-            "lat": first_candidate.metadata.latitude,
-            "lng": first_candidate.metadata.longitude}
-
-def extract_address(text: str):
-    """
-    Given arbitrary *text*, returns a result from the SmartyStreet's US Extract
-    geocoding API containing information about an address connected to the text.
-
-    Note that this API is not consistent with the US Street API, and the lookup
-    and responses must be handled differently.
-    """
-    client = smartystreets_client_builder().build_us_extract_api_client()
-    lookup = ExtractLookup()
-    lookup.text = text
-
-    result = client.send(lookup)
-    metadata = result.metadata
-    
-    addresses = result.addresses
-    for address in addresses:
-        return address.candidates
-
-def standard_address(candidate) -> str:
-    """
-    Given a result object *candidate* from SmartyStreets geocoding API, return a 
-    standardized address.
-    """
-    standard_address = candidate.delivery_line_1
-    if candidate.delivery_line_2:
-        standard_address += ' ' + candidate.delivery_line_2
-
-    return standard_address + ' ' + candidate.last_line
 
 def load_geojson(geojson_filename):
     """Read GeoJSON file and return a list of features converted to shapes."""
